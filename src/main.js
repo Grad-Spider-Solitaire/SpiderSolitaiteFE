0de1e48a7f20a15410bbdb8082e0120885ea66ad
Liam Quick <liam.quick@vcontractor.co.za>
2024-05-06 07:59:32 +0200

    Adding animation for deck dealing

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import {createCardElement, Suit, Card, flipCard} from "../components/card/card.js";
 import {validateForSelect} from "./eventHandlers.js";
+import {cardMoveStyling, moveCard} from "./styling.js";
 
 
 /**
@@ -18,7 +19,7 @@ const cards = Array.from({length: 8}, (_, i) => Array.from({length: 13}, (_, val
 const deck = await Promise.all(cards.map(async card => ({card, element: await createCardElement(card)})));
 deck.sort(() => Math.random() - .5); // shuffling
 
-const initialDeal = Array.from({length: 54}, () => deck.pop());
+const initialDeal = Array.from({length: 10}, () => deck.pop());
 
 /**
  * @type {Array<Array<{card: Card, element: HTMLButtonElement}>>}
@@ -27,27 +28,40 @@ export const boardState = [[],[],[],[],[],[],[],[],[],[]];
 
 const deckDom = document.getElementById('deck');
 deck.forEach(({element}) => {
-  element.disabled = true;
-  element.children.item(0).style.rotate = 'Y .5turn';
-  element.style.pointerEvents = 'none'
+  flipCard(element);
   deckDom.appendChild(element);
 });
 
-deckDom.addEventListener('click', () => {
-  boardState.forEach(col => {
+deckDom.addEventListener('click', event => {
+  deckDom.style.pointerEvents = 'none';
+  event.target.blur();
+  boardState.forEach((col, index, array) => {
     const card = deck.pop();
-    card.element.disabled = false;
-    card.element.style.removeProperty('pointer-events');
-    card.element.children.item(0).style.removeProperty('rotate');
+    const transitionDuration = 1000;
+    card.element.style.setProperty('--transition-duration-ms', `${transitionDuration}ms`);
+    cardMoveStyling(card.element);
+    card.element.style.margin = '0';
     const last = col.at(-1);
-    const colDom = last.element.parentElement;
-    if (!last.card) {
-      colDom.removeChild(last.element);
-      col.pop();
-    }
-    colDom.appendChild(card.element);
-    col.push(card);
-    validateForSelect(col);
+    setTimeout(() => {
+      flipCard(card.element, false);
+      const yOffset = `calc(${last.element.getBoundingClientRect().top}px + ${!last.card ? '0px' : 'calc(var(--card-height) * .2)'})`;
+      moveCard(card.element, `${last.element.getBoundingClientRect().left}px`, yOffset);
+      setTimeout(() => {
+        const colDom = last.element.parentElement;
+        cardMoveStyling(card.element, false);
+        card.element.style.removeProperty('margin');
+        if (!last.card) {
+          colDom.removeChild(last.element);
+          col.pop();
+        }
+        colDom.appendChild(card.element);
+        col.push(card);
+        validateForSelect(col);
+        if (index +1 === array.length && deck.length !== 0) {
+          deckDom.style.removeProperty('pointer-events');
+        }
+      }, transitionDuration);
+    }, 150 * index);
   });
   if (deck.length === 0) deckDom.style.pointerEvents = 'none';
 });
---
+++ src/styling.js
@@ -0,0 +1,19 @@
+export const cardMoveStyling = (element, apply = true) => {
+  if (apply) {
+    element.style.position = 'absolute';
+    element.style.zIndex = '1';
+    element.style.top = `0`;
+    element.style.left = `0`;
+  } else {
+    element.style.removeProperty('position');
+    element.style.removeProperty('top');
+    element.style.removeProperty('pointer-events');
+    element.style.removeProperty('left');
+    element.style.removeProperty('z-index');
+  }
+}
+
+export const moveCard = (element, x, y) => {
+  element.style.top = y;
+  element.style.left = x;
+}
